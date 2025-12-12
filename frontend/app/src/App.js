7dd981f0e3824a6acc71cc0aaf1e3c51476066d4
WRLX <danir1406ildarovich@gmail.com>
2025-12-12 19:37:32 +0500

    frontend+

--- frontend/app/src/App.js
+++ frontend/app/src/App.js
@@ -1,18 +1,54 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import LoginForm from "./components/LoginForm";
+import { Context } from "./index";
+import { observer } from "mobx-react-lite";
+import UserService from "./services/UserService";
 
-function App() {
-  return (
-    <div className="App">
-      <header>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    const { store } = useContext(Context);
+    const [users, setUsers] = useState([]);
 
-export default App;
+    useEffect(() => {
+        if (localStorage.getItem('token')) {
+            store.checkAuth()
+        }
+    }, [])
+
+    async function getUsers() {
+        try {
+            const response = await UserService.fetchUsers();
+            setUsers(response.data);
+        } catch (e) {
+            console.log(e);
+        }
+    }
+
+    if (store.isLoading) {
+        return <div>Загрузка...</div>
+    }
+
+    if (!store.isAuth) {
+        return (
+            <div>
+                <LoginForm />
+                <button onClick={getUsers}>Получить пользователей</button>
+            </div>
+        );
+    }
+
+    return (
+        <div>
+            <h1>{store.isAuth ? `Пользователь авторизован ${store.user.email}` : 'АВТОРИЗУЙТЕСЬ'}</h1>
+            <h1>{store.user.isActivated ? 'Аккаунт подтвержден по почте' : 'ПОДТВЕРДИТЕ АККАУНТ!!!!'}</h1>
+            <button onClick={() => store.logout()}>Выйти</button>
+            <div>
+                <button onClick={getUsers}>Получить пользователей</button>
+            </div>
+            {users.map(user =>
+                <div key={user.email}>{user.email}</div>
+            )}
+        </div>
+    );
+};
+
+export default observer(App);
--- frontend/app/src/index.js
+++ frontend/app/src/index.js
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, { createContext } from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import Store from "./store/store";
+
+const store = new Store();
+
+export const Context = createContext({
+    store,
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <Context.Provider value={{ store }}>
+            <App />
+        </Context.Provider>
+    </React.StrictMode>
 );
